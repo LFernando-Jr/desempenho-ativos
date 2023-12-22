f750f7c2b358df5284f2d67e18573279c8bdf9fe
LFernando-Jr <68956886+LFernando-Jr@users.noreply.github.com>
2023-12-22 16:14:11 -0300

    altera a dimensão dos gráficos e adiciona novas legendas

--- desempenho_fundos.R
+++ desempenho_fundos.R
@@ -61,7 +61,7 @@ data <- df |>
   mutate(excess_var_ano = var_ano - cdi_ano) |>
   mutate(excess_var_mes = var_mes - cdi_mes)
 
-tbl <- data[,c(1,2,12,13)] |>
+tbl <- data[,c(1,2,11,12,13)] |>
   arrange(desc(Data)) |>
   group_by(`Nome do Ativo`) |>
   slice(1) |>
@@ -69,44 +69,62 @@ tbl <- data[,c(1,2,12,13)] |>
   select(-Data) |>
   arrange(desc(excess_var_ano)) |>
   rename(`% No mês ` = excess_var_mes,
-         `% No ano ` = excess_var_ano)
+         `% No ano ` = excess_var_ano,
+         `% em 36 meses ` = excess_var_36M )
 
 # write.csv2(data, "desempenho_fundos.csv")
- write.csv2(tbl, "desempenho_fundos.csv")
+# write.csv2(tbl, "desempenho_fundos.csv")
 
 # Visualização de dados ---------------------------------------------------
 
 ## Variação Trianual ------------------------------------------------------
 
 data |> 
-  filter(Data >= "2018-01-01") |>
+  filter(Data >= "2023-01-01") |>
+  mutate(`Nome do Ativo` = factor(`Nome do Ativo`, levels = arrange(tbl, desc(`% em 36 meses `))$`Nome do Ativo`)) |>
   ggplot() +
-  geom_line(data = . %>% filter(`Nome do Ativo` != "IHFA"), 
-            aes(Data, excess_var_36M, colour = `Nome do Ativo`), linewidth = .75) +
-  geom_line(data = . %>% filter(`Nome do Ativo` == "IHFA"), 
-            aes(Data, excess_var_36M, colour = "IHFA"), linewidth = .5, linetype = "longdash") +
+  aes(Data, excess_var_36M, colour = `Nome do Ativo`, linetype = `Nome do Ativo`) +
+  geom_line(linewidth = .75) +
   geom_hline(yintercept = 0) +
   theme_bw() + theme(panel.grid.minor = element_blank(), 
                      axis.line = element_line(colour = "black"),
-                     legend.position = "bottom", 
                      legend.title = element_blank(), 
                      axis.title = element_blank(), 
                      strip.background = element_blank()) + 
-  scale_x_date(expand = c(0,0), date_labels = "%m/%Y", breaks = "6 months",) +
-  scale_colour_manual(values = c("#2F47AD",
-                                 "black",
-                                 "#8C977D",
-                                 "#31AFE0",
-                                 "#E47632",
-                                 "#AD4728",
-                                 "#3BA58B",
-                                 "#D4A83F",
-                                 "#8057A5")) + 
-  labs(title = "Fundos",
+  scale_x_date(expand = c(0,0), date_labels = "%b", breaks = "1 month",) +
+  scale_colour_manual(values = c("ABSOLUTE VERTEX FIC MULTIMERCADO" = "#2F47AD",
+                                 "IHFA" = "black",
+                                 "JGP STRATEGY FIC MULTIMERCADO" = "#8C977D",
+                                 "KINEA ATLAS II FI MULTIMERCADO" = "#31AFE0",
+                                 "SPX NIMITZ FEEDER FIC MULTIMERCADO" = "#E47632",
+                                 "KAPITALO ZETA FIC MULTIMERCADO" = "#AD4728",
+                                 "OCCAM RETORNO ABSOLUTO ADVISORY FIC MULTIMERCADO" = "#3BA58B",
+                                 "LEGACY CAPITAL ADVISORY FIC MULTIMERCADO" = "#D4A83F",
+                                 "VERDE AM X60 ADVISORY FIC MULTIMERCADO" = "#2f5a3d"),
+                      labels = c("ABSOLUTE VERTEX FIC MULTIMERCADO" = paste0("Absolute: ", round(tbl$`% em 36 meses `[which(tbl$`Nome do Ativo` == "ABSOLUTE VERTEX FIC MULTIMERCADO")],2), "%"),
+                                 "IHFA" = paste0("IHFA: ", round(tbl$`% em 36 meses `[which(tbl$`Nome do Ativo` == "IHFA")],2), "%"),
+                                 "JGP STRATEGY FIC MULTIMERCADO" = paste0("JGP: ", round(tbl$`% em 36 meses `[which(tbl$`Nome do Ativo` == "JGP STRATEGY FIC MULTIMERCADO")],2), "%"),
+                                 "KINEA ATLAS II FI MULTIMERCADO" = paste0("Kinea: ", round(tbl$`% em 36 meses `[which(tbl$`Nome do Ativo` == "KINEA ATLAS II FI MULTIMERCADO")],2), "%"),
+                                 "SPX NIMITZ FEEDER FIC MULTIMERCADO" = paste0("SPX: ", round(tbl$`% em 36 meses `[which(tbl$`Nome do Ativo` == "SPX NIMITZ FEEDER FIC MULTIMERCADO")],2), "%"),
+                                 "KAPITALO ZETA FIC MULTIMERCADO" = paste0("Kapital: ", round(tbl$`% em 36 meses `[which(tbl$`Nome do Ativo` == "KAPITALO ZETA FIC MULTIMERCADO")],2), "%"),
+                                 "OCCAM RETORNO ABSOLUTO ADVISORY FIC MULTIMERCADO" = paste0("Occam: ", round(tbl$`% em 36 meses `[which(tbl$`Nome do Ativo` == "OCCAM RETORNO ABSOLUTO ADVISORY FIC MULTIMERCADO")],2), "%"),
+                                 "LEGACY CAPITAL ADVISORY FIC MULTIMERCADO" = paste0("Legacy: ", round(tbl$`% em 36 meses `[which(tbl$`Nome do Ativo` == "LEGACY CAPITAL ADVISORY FIC MULTIMERCADO")],2), "%"),
+                                 "VERDE AM X60 ADVISORY FIC MULTIMERCADO" = paste0("Verde: ", round(tbl$`% em 36 meses `[which(tbl$`Nome do Ativo` == "VERDE AM X60 ADVISORY FIC MULTIMERCADO")],2), "%"))) + 
+  scale_linetype_manual(values = c("ABSOLUTE VERTEX FIC MULTIMERCADO" = "solid",
+                                 "IHFA" = "longdash",
+                                 "JGP STRATEGY FIC MULTIMERCADO" = "solid",
+                                 "KINEA ATLAS II FI MULTIMERCADO" = "solid",
+                                 "SPX NIMITZ FEEDER FIC MULTIMERCADO" = "solid",
+                                 "KAPITALO ZETA FIC MULTIMERCADO" = "solid",
+                                 "OCCAM RETORNO ABSOLUTO ADVISORY FIC MULTIMERCADO" = "solid",
+                                 "LEGACY CAPITAL ADVISORY FIC MULTIMERCADO" = "solid",
+                                 "VERDE AM X60 ADVISORY FIC MULTIMERCADO" = "solid")) +
+  guides(linetype = "none") +
+  labs(title = NULL,
        subtitle = "Variação trianual do excesso de retorno", 
        caption = "Fonte: Capri com dados da Quantum Axis")
   
-ggsave("variacao trianual.png", width = 15, height = 8.661, units = "in", dpi = 800, path = paste(getwd(),
+ggsave("variacao trianual.png", width = 4800, height = 2160, units = "px", dpi = 576, path = paste(getwd(),
                                                                                                    "/Gráficos/Fundos",
                                                                                                    sep = ""))
 
@@ -114,33 +132,50 @@ ggsave("variacao trianual.png", width = 15, height = 8.661, units = "in", dpi = 
 
 data |> 
   filter(Data >= "2023-01-01") |>
+  mutate(`Nome do Ativo` = factor(`Nome do Ativo`, levels = arrange(tbl, desc(`% No ano `))$`Nome do Ativo`)) |>
   ggplot() +
-  geom_line(data = . %>% filter(`Nome do Ativo` != "IHFA"), 
-            aes(Data, excess_var_ano, colour = `Nome do Ativo`), linewidth = .75) +
-  geom_line(data = . %>% filter(`Nome do Ativo` == "IHFA"), 
-            aes(Data, excess_var_ano, colour = "IHFA"), linewidth = .5, linetype = "longdash") +
+  aes(Data, excess_var_ano, colour = `Nome do Ativo`, linetype = `Nome do Ativo`) +
+  geom_line(linewidth = .75) +
   geom_hline(yintercept = 0) +
   theme_bw() + theme(panel.grid.minor = element_blank(), 
                      axis.line = element_line(colour = "black"),
-                     legend.position = "bottom", 
                      legend.title = element_blank(), 
                      axis.title = element_blank(), 
                      strip.background = element_blank()) + 
   scale_x_date(expand = c(0,0), date_labels = "%b", breaks = "1 months",) +
-  scale_colour_manual(values = c("#2F47AD",
-                                 "black",
-                                 "#8C977D",
-                                 "#31AFE0",
-                                 "#E47632",
-                                 "#AD4728",
-                                 "#3BA58B",
-                                 "#D4A83F",
-                                 "#8057A5")) + 
-  labs(title = "Fundos",
+  scale_colour_manual(values = c("ABSOLUTE VERTEX FIC MULTIMERCADO" = "#2F47AD",
+                                 "IHFA" = "black",
+                                 "JGP STRATEGY FIC MULTIMERCADO" = "#8C977D",
+                                 "KINEA ATLAS II FI MULTIMERCADO" = "#31AFE0",
+                                 "SPX NIMITZ FEEDER FIC MULTIMERCADO" = "#E47632",
+                                 "KAPITALO ZETA FIC MULTIMERCADO" = "#AD4728",
+                                 "OCCAM RETORNO ABSOLUTO ADVISORY FIC MULTIMERCADO" = "#3BA58B",
+                                 "LEGACY CAPITAL ADVISORY FIC MULTIMERCADO" = "#D4A83F",
+                                 "VERDE AM X60 ADVISORY FIC MULTIMERCADO" = "#2f5a3d"),
+                      labels = c("ABSOLUTE VERTEX FIC MULTIMERCADO" = paste0("Absolute: ", round(tbl$`% No ano `[which(tbl$`Nome do Ativo` == "ABSOLUTE VERTEX FIC MULTIMERCADO")],2), "%"),
+                                 "IHFA" = paste0("IHFA: ", round(tbl$`% No ano `[which(tbl$`Nome do Ativo` == "IHFA")],2), "%"),
+                                 "JGP STRATEGY FIC MULTIMERCADO" = paste0("JGP: ", round(tbl$`% No ano `[which(tbl$`Nome do Ativo` == "JGP STRATEGY FIC MULTIMERCADO")],2), "%"),
+                                 "KINEA ATLAS II FI MULTIMERCADO" = paste0("Kinea: ", round(tbl$`% No ano `[which(tbl$`Nome do Ativo` == "KINEA ATLAS II FI MULTIMERCADO")],2), "%"),
+                                 "SPX NIMITZ FEEDER FIC MULTIMERCADO" = paste0("SPX: ", round(tbl$`% No ano `[which(tbl$`Nome do Ativo` == "SPX NIMITZ FEEDER FIC MULTIMERCADO")],2), "%"),
+                                 "KAPITALO ZETA FIC MULTIMERCADO" = paste0("Kapital: ", round(tbl$`% No ano `[which(tbl$`Nome do Ativo` == "KAPITALO ZETA FIC MULTIMERCADO")],2), "%"),
+                                 "OCCAM RETORNO ABSOLUTO ADVISORY FIC MULTIMERCADO" = paste0("Occam: ", round(tbl$`% No ano `[which(tbl$`Nome do Ativo` == "OCCAM RETORNO ABSOLUTO ADVISORY FIC MULTIMERCADO")],2), "%"),
+                                 "LEGACY CAPITAL ADVISORY FIC MULTIMERCADO" = paste0("Legacy: ", round(tbl$`% No ano `[which(tbl$`Nome do Ativo` == "LEGACY CAPITAL ADVISORY FIC MULTIMERCADO")],2), "%"),
+                                 "VERDE AM X60 ADVISORY FIC MULTIMERCADO" = paste0("Verde: ", round(tbl$`% No ano `[which(tbl$`Nome do Ativo` == "VERDE AM X60 ADVISORY FIC MULTIMERCADO")],2), "%"))) + 
+  scale_linetype_manual(values = c("ABSOLUTE VERTEX FIC MULTIMERCADO" = "solid",
+                                   "IHFA" = "longdash",
+                                   "JGP STRATEGY FIC MULTIMERCADO" = "solid",
+                                   "KINEA ATLAS II FI MULTIMERCADO" = "solid",
+                                   "SPX NIMITZ FEEDER FIC MULTIMERCADO" = "solid",
+                                   "KAPITALO ZETA FIC MULTIMERCADO" = "solid",
+                                   "OCCAM RETORNO ABSOLUTO ADVISORY FIC MULTIMERCADO" = "solid",
+                                   "LEGACY CAPITAL ADVISORY FIC MULTIMERCADO" = "solid",
+                                   "VERDE AM X60 ADVISORY FIC MULTIMERCADO" = "solid")) +
+  guides(linetype = "none") +
+  labs(title = NULL,
        subtitle = "Excesso de retorno acumulado no ano",
        caption = "Fonte: Capri com dados da Quantum Axis")
 
-ggsave("variacao anual acumulada.png", width = 15, height = 8.661, units = "in", dpi = 800, path = paste(getwd(),
+ggsave("variacao anual acumulada.png", width = 4800, height = 2160, units = "px", dpi = 576, path = paste(getwd(),
                                                                                                           "/Gráficos/Fundos",
                                                                                                           sep = ""))
 
@@ -148,32 +183,48 @@ ggsave("variacao anual acumulada.png", width = 15, height = 8.661, units = "in",
 
 data |> 
   filter(Data >= "2023-11-30") |>
+  mutate(`Nome do Ativo` = factor(`Nome do Ativo`, levels = arrange(tbl, desc(`% No mês `))$`Nome do Ativo`)) |>
   ggplot() +
-  geom_line(data = . %>% filter(`Nome do Ativo` != "IHFA"), 
-            aes(Data, excess_var_mes, colour = `Nome do Ativo`), linewidth = .75) +
-  geom_line(data = . %>% filter(`Nome do Ativo` == "IHFA"), 
-            aes(Data, excess_var_mes, colour = "IHFA"), linewidth = .5, linetype = "longdash") +
-  geom_hline(yintercept = 0) +
+  aes(Data, excess_var_mes, colour = `Nome do Ativo`, linetype = `Nome do Ativo`) +
+  geom_line(linewidth = .75) +geom_hline(yintercept = 0) +
   theme_bw() + theme(panel.grid.minor = element_blank(), 
                      axis.line = element_line(colour = "black"),
-                     legend.position = "bottom", 
                      legend.title = element_blank(), 
                      axis.title = element_blank(), 
                      strip.background = element_blank()) + 
   scale_x_date(expand = c(0,0), date_labels = "%d", breaks = "1 day",) +
-  scale_colour_manual(values = c("#2F47AD",
-                                 "black",
-                                 "#8C977D",
-                                 "#31AFE0",
-                                 "#E47632",
-                                 "#AD4728",
-                                 "#3BA58B",
-                                 "#D4A83F",
-                                 "#8057A5")) + 
-  labs(title = "Fundos",
+  scale_colour_manual(values = c("ABSOLUTE VERTEX FIC MULTIMERCADO" = "#2F47AD",
+                                 "IHFA" = "black",
+                                 "JGP STRATEGY FIC MULTIMERCADO" = "#8C977D",
+                                 "KINEA ATLAS II FI MULTIMERCADO" = "#31AFE0",
+                                 "SPX NIMITZ FEEDER FIC MULTIMERCADO" = "#E47632",
+                                 "KAPITALO ZETA FIC MULTIMERCADO" = "#AD4728",
+                                 "OCCAM RETORNO ABSOLUTO ADVISORY FIC MULTIMERCADO" = "#3BA58B",
+                                 "LEGACY CAPITAL ADVISORY FIC MULTIMERCADO" = "#D4A83F",
+                                 "VERDE AM X60 ADVISORY FIC MULTIMERCADO" = "#2f5a3d"),
+                      labels = c("ABSOLUTE VERTEX FIC MULTIMERCADO" = paste0("Absolute: ", round(tbl$`% No mês `[which(tbl$`Nome do Ativo` == "ABSOLUTE VERTEX FIC MULTIMERCADO")],2), "%"),
+                                 "IHFA" = paste0("IHFA: ", round(tbl$`% No mês `[which(tbl$`Nome do Ativo` == "IHFA")],2), "%"),
+                                 "JGP STRATEGY FIC MULTIMERCADO" = paste0("JGP: ", round(tbl$`% No mês `[which(tbl$`Nome do Ativo` == "JGP STRATEGY FIC MULTIMERCADO")],2), "%"),
+                                 "KINEA ATLAS II FI MULTIMERCADO" = paste0("Kinea: ", round(tbl$`% No mês `[which(tbl$`Nome do Ativo` == "KINEA ATLAS II FI MULTIMERCADO")],2), "%"),
+                                 "SPX NIMITZ FEEDER FIC MULTIMERCADO" = paste0("SPX: ", round(tbl$`% No mês `[which(tbl$`Nome do Ativo` == "SPX NIMITZ FEEDER FIC MULTIMERCADO")],2), "%"),
+                                 "KAPITALO ZETA FIC MULTIMERCADO" = paste0("Kapital: ", round(tbl$`% No mês `[which(tbl$`Nome do Ativo` == "KAPITALO ZETA FIC MULTIMERCADO")],2), "%"),
+                                 "OCCAM RETORNO ABSOLUTO ADVISORY FIC MULTIMERCADO" = paste0("Occam: ", round(tbl$`% No mês `[which(tbl$`Nome do Ativo` == "OCCAM RETORNO ABSOLUTO ADVISORY FIC MULTIMERCADO")],2), "%"),
+                                 "LEGACY CAPITAL ADVISORY FIC MULTIMERCADO" = paste0("Legacy: ", round(tbl$`% No mês `[which(tbl$`Nome do Ativo` == "LEGACY CAPITAL ADVISORY FIC MULTIMERCADO")],2), "%"),
+                                 "VERDE AM X60 ADVISORY FIC MULTIMERCADO" = paste0("Verde: ", round(tbl$`% No mês `[which(tbl$`Nome do Ativo` == "VERDE AM X60 ADVISORY FIC MULTIMERCADO")],2), "%"))) + 
+  scale_linetype_manual(values = c("ABSOLUTE VERTEX FIC MULTIMERCADO" = "solid",
+                                   "IHFA" = "longdash",
+                                   "JGP STRATEGY FIC MULTIMERCADO" = "solid",
+                                   "KINEA ATLAS II FI MULTIMERCADO" = "solid",
+                                   "SPX NIMITZ FEEDER FIC MULTIMERCADO" = "solid",
+                                   "KAPITALO ZETA FIC MULTIMERCADO" = "solid",
+                                   "OCCAM RETORNO ABSOLUTO ADVISORY FIC MULTIMERCADO" = "solid",
+                                   "LEGACY CAPITAL ADVISORY FIC MULTIMERCADO" = "solid",
+                                   "VERDE AM X60 ADVISORY FIC MULTIMERCADO" = "solid")) +
+  guides(linetype = "none") +
+  labs(title = NULL,
        subtitle = "Excesso de retorno acumulado no mês",
        caption = "Fonte: Capri com dados da Quantum Axis")
 
-ggsave("variacao mensal acumulada.png", width = 15, height = 8.661, units = "in", dpi = 800, path = paste(getwd(),
+ggsave("variacao mensal acumulada.png", width = 4800, height = 2160, units = "px", dpi = 576, path = paste(getwd(),
                                                                                                            "/Gráficos/Fundos",
                                                                                                            sep = ""))
--- desempenho_indices.R
+++ desempenho_indices.R
@@ -59,7 +59,6 @@ data |>
             aes(Data, var_12M, colour = "CDI"), linewidth = .5, linetype = "longdash") +
   theme_bw() + theme(panel.grid.minor = element_blank(), 
                      axis.line = element_line(colour = "black"),
-                     legend.position = "bottom", 
                      legend.title = element_blank(), 
                      axis.title = element_blank(), 
                      strip.background = element_blank()) + 
@@ -77,40 +76,59 @@ data |>
        subtitle = "Variação anual",
        caption = "Fonte: Capri com dados da Quantum Axis")
 
-ggsave("variacao anual.png", width = 15, height = 8.661, units = "in", dpi = 800, path = paste(getwd(),
+ggsave("variacao anual.png", width = 4800, height = 2160, units = "px", dpi = 576, path = paste(getwd(),
                                                                                                 "/Gráficos/Índices",
                                                                                                 sep = ""))
 
+# ggsave("variacao anual.png", width = 15, height = 8.661, units = "in", dpi = 800, path = paste(getwd(),
+#                                                                                                           "/Gráficos/Fundos",
+#                                                                                                           sep = ""))
+
 ## Variação anual acumulada  -------------------------------------------------------
 
-data |> 
+data |>
   filter(Data >= "2023-01-01") |>
+  mutate(`Nome do Ativo` = factor(`Nome do Ativo`, levels = arrange(tbl, desc(`% No ano `))$`Nome do Ativo`)) |>
   ggplot() +
-  geom_line(data = . %>% filter(`Nome do Ativo` != "CDI"), 
-            aes(Data, var_ano, colour = `Nome do Ativo`), linewidth = .75) +
-  geom_line(data = . %>% filter(`Nome do Ativo` == "CDI"), 
-            aes(Data, var_ano, colour = "CDI"), linewidth = .5, linetype = "longdash") +
+  aes(Data, var_ano, colour = `Nome do Ativo`, linetype = `Nome do Ativo`) +
+  geom_line(linewidth = .75) +
   theme_bw() + theme(panel.grid.minor = element_blank(), 
                      axis.line = element_line(colour = "black"),
-                     legend.position = "bottom", 
                      legend.title = element_blank(), 
                      axis.title = element_blank(), 
                      strip.background = element_blank()) + 
-  scale_x_date(expand = c(0,0), date_labels = "%b", breaks = "1 months",) +
-  scale_colour_manual(values = c("black",
-                                 "#2F47AD",
-                                 "#8C977D",
-                                 "#31AFE0",
-                                 "#E47632",
-                                 "#AD4728",
-                                 "#3BA58B",
-                                 "#D4A83F",
-                                 "#8057A5")) + 
-  labs(title = "Índices",
+  scale_x_date(expand = c(0,0), date_labels = "%b", breaks = "1 months") +
+  scale_colour_manual(values = c("Ibovespa" = "#2F47AD",
+                                 "CDI" = "black",
+                                 "IDA-DI" = "#8C977D",
+                                 "Idex-CDI Geral JGP" = "#31AFE0",
+                                 "IHFA" = "#E47632",
+                                 "IMA-B" = "#AD4728",
+                                 "IMA-B 5" = "#3BA58B",
+                                 "IRF-M" = "#D4A83F"),
+                      labels = c("Ibovespa" = paste0("Ibovespa: ", tbl$`% No ano `[which(tbl$`Nome do Ativo` == "Ibovespa")], "%"),
+                               "CDI" = paste0("CDI: ", tbl$`% No ano `[which(tbl$`Nome do Ativo` == "CDI")], "%"),
+                               "IDA-DI" = paste0("IDA-DI: ", tbl$`% No ano `[which(tbl$`Nome do Ativo` == "IDA-DI")], "%"),
+                               "Idex-CDI Geral JGP" = paste0("IDEX: ", tbl$`% No ano `[which(tbl$`Nome do Ativo` == "Idex-CDI Geral JGP")], "%"),
+                               "IHFA" = paste0("IHFA: ", tbl$`% No ano `[which(tbl$`Nome do Ativo` == "IHFA")], "%"),
+                               "IMA-B" = paste0("IMA-B: ", tbl$`% No ano `[which(tbl$`Nome do Ativo` == "IMA-B")], "%"),
+                               "IMA-B 5" = paste0("IMA-B 5: ", tbl$`% No ano `[which(tbl$`Nome do Ativo` == "IMA-B 5")], "%"),
+                               "IRF-M" = paste0("IRF-M: ", tbl$`% No ano `[which(tbl$`Nome do Ativo` == "IRF-M")], "%"))) +
+  scale_linetype_manual(values = c("CDI" = "longdash", 
+                                   "Ibovespa" = "solid",
+                                   "CDI" = "solid",
+                                   "IDA-DI" = "solid",
+                                   "Idex-CDI Geral JGP" = "solid",
+                                   "IHFA" = "solid",
+                                   "IMA-B" = "solid",
+                                   "IMA-B 5" = "solid",
+                                   "IRF-M" = "solid")) +
+  guides(linetype = "none") +
+  labs(title = NULL,
        subtitle = "Variação acumulada no ano", 
        caption = "Fonte: Capri com dados da Quantum Axis")
 
-ggsave("variacao anual acumulada.png", width = 15, height = 8.661, units = "in", dpi = 800, path = paste(getwd(),
+ggsave("variacao anual acumulada.png", width = 4800, height = 2160, units = "px", dpi = 576, path = paste(getwd(),
                                                                                                           "/Gráficos/Índices",
                                                                                                           sep = ""))
 
@@ -118,31 +136,46 @@ ggsave("variacao anual acumulada.png", width = 15, height = 8.661, units = "in",
 
 data |>
   filter(Data >= "2023-11-30") |>
+  mutate(`Nome do Ativo` = factor(`Nome do Ativo`, levels = arrange(tbl, desc(`% No mês `))$`Nome do Ativo`)) |>
   ggplot() +
-  geom_line(data = . %>% filter(`Nome do Ativo` != "CDI"),
-            aes(Data, var_mes, colour = `Nome do Ativo`), linewidth = .75) +
-  geom_line(data = . %>% filter(`Nome do Ativo` == "CDI"),
-            aes(Data, var_mes, colour = "CDI"), linewidth = .5, linetype = "longdash") +
+  aes(Data, var_mes, colour = `Nome do Ativo`, linetype = `Nome do Ativo`) +
+  geom_line(linewidth = .75) +
   theme_bw() + theme(panel.grid.minor = element_blank(),
                      axis.line = element_line(colour = "black"),
-                     legend.position = "bottom",
                      legend.title = element_blank(),
                      axis.title = element_blank(),
                      strip.background = element_blank()) +
   scale_x_date(expand = c(0,0), date_labels = "%d", breaks = "1 day",) +
-  scale_colour_manual(values = c("black",
-                                 "#2F47AD",
-                                 "#8C977D",
-                                 "#31AFE0",
-                                 "#E47632",
-                                 "#AD4728",
-                                 "#3BA58B",
-                                 "#D4A83F",
-                                 "#8057A5")) +
-  labs(title = "Índices",
+  scale_colour_manual(values = c("Ibovespa" = "#2F47AD",
+                                 "CDI" = "black",
+                                 "IDA-DI" = "#8C977D",
+                                 "Idex-CDI Geral JGP" = "#31AFE0",
+                                 "IHFA" = "#E47632",
+                                 "IMA-B" = "#AD4728",
+                                 "IMA-B 5" = "#3BA58B",
+                                 "IRF-M" = "#D4A83F"),
+                      labels = c("Ibovespa" = paste0("Ibovespa: ", tbl$`% No mês `[which(tbl$`Nome do Ativo` == "Ibovespa")], "%"),
+                                 "CDI" = paste0("CDI: ", tbl$`% No mês `[which(tbl$`Nome do Ativo` == "CDI")], "%"),
+                                 "IDA-DI" = paste0("IDA-DI: ", tbl$`% No mês `[which(tbl$`Nome do Ativo` == "IDA-DI")], "%"),
+                                 "Idex-CDI Geral JGP" = paste0("IDEX: ", tbl$`% No mês `[which(tbl$`Nome do Ativo` == "Idex-CDI Geral JGP")], "%"),
+                                 "IHFA" = paste0("IHFA: ", tbl$`% No mês `[which(tbl$`Nome do Ativo` == "IHFA")], "%"),
+                                 "IMA-B" = paste0("IMA-B: ", tbl$`% No mês `[which(tbl$`Nome do Ativo` == "IMA-B")], "%"),
+                                 "IMA-B 5" = paste0("IMA-B 5: ", tbl$`% No mês `[which(tbl$`Nome do Ativo` == "IMA-B 5")], "%"),
+                                 "IRF-M" = paste0("IRF-M: ", tbl$`% No mês `[which(tbl$`Nome do Ativo` == "IRF-M")], "%"))) +
+  scale_linetype_manual(values = c("CDI" = "longdash", 
+                                   "Ibovespa" = "solid",
+                                   "CDI" = "solid",
+                                   "IDA-DI" = "solid",
+                                   "Idex-CDI Geral JGP" = "solid",
+                                   "IHFA" = "solid",
+                                   "IMA-B" = "solid",
+                                   "IMA-B 5" = "solid",
+                                   "IRF-M" = "solid")) +
+  guides(linetype = "none") +
+  labs(title = NULL,
        subtitle = "Variação acumulada no mês", 
        caption = "Fonte: Capri com dados da Quantum Axis")
 
-ggsave("variacao mensal acumulada.png", width = 15, height = 8.661, units = "in", dpi = 800, path = paste(getwd(),
+ggsave("variacao mensal acumulada.png", width = 9720, height = 3920, units = "px", dpi = 1152, path = paste(getwd(),
                                                                                                            "/Gráficos/Índices",
                                                                                                            sep = ""))
